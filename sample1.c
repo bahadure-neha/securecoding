f93a81d6808154991af70a489632bf36b264ecf0
bahadure-neha <32226035+bahadure-neha@users.noreply.github.com>
2025-01-21 23:27:56 -0500

    Update sample1.c

--- sample1.c
+++ sample1.c
@@ -1,7 +1,17 @@
-def get_update_bmi_query(username, weight, height):
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+ 
+#define BUFSIZE 256
     
-    bmi = weight / (height * height)
-
-    query = f"UPDATE users SET bmi = {bmi} WHERE username = '{username}'"
-
-    print(query)
+// This program prints the size of a specified file in bytes
+int main(int argc, char** argv) {
+    // Ensure that the user supplied exactly one command line argument
+    if (argc != 2) { 
+        fprintf(stderr, "Please provide the address of a file as an input.\n");
+        return -1;
+    }
+    char cmd[BUFSIZE] = "wc -c < ";
+    strcat(cmd, argv[1]);
+    system(cmd);
+}
